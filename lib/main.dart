b168ada898f319def3a0c115b42dd89a01b3abb8
BitNers <wesleyalvessilva@outlook.com.br>
2021-10-20 00:03:38 -0300

    Update main.dart
    Updating main menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,248 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'dart:core';
+import 'controllers/restapi.dart' as restapi;
+import 'dart:developer';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainMenu());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainMenu extends StatelessWidget {
+  const MainMenu({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      theme: ThemeData.dark(),
+      home: Scaffold(
+
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+
+        ),
+          body: Body()
+
+        )
+      );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Body> createState() => _BodyState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _BodyState extends State<Body> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return SafeArea(
+      child: Column(
+        children: [
+          NowPlayingWidget(),
+          PopularsWidget()
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+class NowPlayingWidget extends StatefulWidget {
+  const NowPlayingWidget({Key? key}) : super(key: key);
+
+  @override
+  _NowPlayingWidgetState createState() => _NowPlayingWidgetState();
+}
+
+class _NowPlayingWidgetState extends State<NowPlayingWidget> {
+  @override
+
+
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder(
+        future:  new restapi.RestAPI().get_now_playing(),
+        builder:(context, snapshot) {
+          if(snapshot.hasData){
+            var dd = json.decode(snapshot.data.toString());
+
+            return Container(
+                padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+              child: Column(
+                children: [
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child:
+
+                      Text("Últimos filmes em Cartazes",
+                      textAlign: TextAlign.left,
+
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                    ),),
+                  ),
+                  SizedBox(
+                    height: 18.0,
+                  ),
+                  Container(
+                    height: 280.0,
+                    child: ListView.builder(
+                        itemCount: dd['results'].length,
+                        itemBuilder: (BuildContext ctx, int idx){
+                          return Card(
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Text(dd['results'][idx]['title']),
+                            ),
+                          );
+                        }
+                    ),
+                  ),
+                ],
+              )
+
+
+            );
+
+          }else{
+            return Center(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0.0,80.0,0.0, 15.0),
+                    child: CircularProgressIndicator(
+                      color: Colors.red,
+                    ),
+                  ),
+                  Text("Loading now playing movies...",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 18.0,
+                        letterSpacing: 2.0,
+                    ),),
+                ],
+              ),
+            );
+          }
+        },
+      )
+
+    );
+  }
+}
+
+class PopularsWidget extends StatefulWidget {
+  const PopularsWidget({Key? key}) : super(key: key);
+
+  @override
+  _PopularsWidgetState createState() => _PopularsWidgetState();
+}
+
+class _PopularsWidgetState extends State<PopularsWidget> {
+
+  String urlImage = new restapi.RestAPI().get_urlBaseImage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: FutureBuilder(
+          future:  new restapi.RestAPI().get_now_playing(),
+          builder:(context, snapshot) {
+            if(snapshot.hasData){
+              var dd = json.decode(snapshot.data.toString());
+
+              return Container(
+                  padding: EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 0.0),
+                  child: Column(
+                    children: [
+
+                      Align(
+                        alignment: Alignment.centerLeft,
+
+                        child:
+                        Text("Populares",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                      Container(
+                        height: 200.0,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                            itemCount: dd['results'].length,
+                            itemBuilder: (BuildContext ctx, int idx){
+                              return Container(
+                                height: double.infinity,
+                                width: 125,
+
+                                padding: EdgeInsets.symmetric(vertical: 0.0, horizontal:8.0),
+                                child: Column(
+                                  children: [
+                                    Card(
+
+                                    shadowColor: Colors.white,
+                                      child: Padding(
+                                        padding: EdgeInsets.all(1.0),
+                                        child: Image.network(urlImage + dd['results'][idx]['poster_path']),
+                                      ),
+                                    ),
+                                    Text(dd['results'][idx]['title'],
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                    fontWeight: FontWeight.w400,
+                                      fontSize: 11.0
+
+                                    ),),
+                                  ],
+                                ),
+                              );
+                            }
+                        ),
+                      ),
+                    ],
+                  )
+
+
+              );
+
+            }else{
+              return Center(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0.0,80.0,0.0, 15.0),
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    ),
+                    Text("Loading popular movies...",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 18.0,
+                        letterSpacing: 2.0,
+                      ),),
+                  ],
+                ),
+              );
+            }
+          },
+        )
+
+    );
+  }
+}
+
+
